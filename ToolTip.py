5b893cd12beeecf065434a6396fd02bebca73ae1
leia <1102005412@qq.com>
2025-09-04 22:14:41 +0800

    修改ToolTip实现： 1.支持修改提示文字（支持多次设置） 2.新加Destroy 事件处理，解决widget失效时由于双向引用导致回收数据

--- ToolTip.py
+++ ToolTip.py
@@ -1,23 +1,28 @@
 import tkinter as tk
-from tkinter import ttk
+from tkinter import StringVar
 
-class ToolTip:
+class ToolTipImp:
     def __init__(self, widget, text):
-        self.widget = widget
         self.tooltip = None
-        self.text = text
+        self.text = StringVar(value = text)
+        widget.tooltip = self
+        widget.bind('<Enter>', self.show_tooltip)
+        widget.bind('<Leave>', self.hide_tooltip)
+        # widget 销毁时解除绑定
+        widget.bind('<Destroy>', self.unbind)
 
-        self.widget.bind('<Enter>', self.show_tooltip)
-        self.widget.bind('<Leave>', self.hide_tooltip)
+    # def __del__(self):
+    #     print("delete " + str(self))
 
     def show_tooltip(self, event):
-        x, y, cx, cy = self.widget.bbox('insert')
-        x = x + self.widget.winfo_rootx() + 25
-        y = y + cy + self.widget.winfo_rooty() + 25
+        widget = event.widget
+        x, y, cx, cy = widget.bbox('insert')
+        x = x + widget.winfo_rootx() + 25
+        y = y + cy + widget.winfo_rooty() + 25
 
         if self.tooltip is None:
-            self.tooltip = tk.Toplevel(self.widget)
-            label = tk.Label(self.tooltip, text=self.text, justify='left', background='#ffffe0', relief='solid', borderwidth=1, font=('楷体', '10', 'normal'))
+            self.tooltip = tk.Toplevel(widget)
+            label = tk.Label(self.tooltip, textvariable=self.text, justify='left', background='#ffffe0', relief='solid', borderwidth=1, font=('楷体', '10', 'normal'))
             label.pack(ipadx=1)
 
         self.tooltip.overrideredirect(True) 
@@ -27,3 +32,19 @@ class ToolTip:
     def hide_tooltip(self, event):
         if self.tooltip:
             self.tooltip.withdraw()
+            
+    def unbind(self,event):
+        widget = event.widget
+        widget.unbind('<Enter>')
+        widget.unbind('<Leave>')
+        widget.unbind('<Destroy>')
+        self.tooltip.destroy()
+        self.tooltip = None
+        widget.tooltip = None
+
+def ToolTip(widget, text):
+    #防止重复绑定
+    if hasattr(widget,"tooltip"):
+        widget.tooltip.text.set(text)
+        return
+    widget.tooltip = ToolTipImp(widget, text)
